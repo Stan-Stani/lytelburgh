bd2939f9304a63b2a47017827ec63afdad3c4c26
Stan <cyclinglinguist@gmail.com>
2023-08-17 20:54:36 -0500

    feat: basic integration of client w/ server
    (Using nswag studio to generate ts client)

--- lytelburghClient/src/App.tsx
+++ lytelburghClient/src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Client } from "./api/lytelburghApi";
+
+const api = new Client("https://localhost:7146");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,18 +21,23 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={async () => {
+            setCount((count) => count + 1);
+            console.log(await api.weatherForecast())
+          }}
+        >
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/sApp.sadftsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- lytelburghServer/webapi/Program.cs
+++ lytelburghServer/webapi/Program.cs
@@ -6,14 +6,41 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApiDocument();
+
+
+
+#if DEBUG
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "debug-cors",
+                      policy =>
+                      {
+                          policy.WithOrigins("*")
+                                .AllowAnyMethod()
+                                .AllowAnyHeader(); 
+                      });
+});
+#endif
+
+
 
 var app = builder.Build();
+
+#if DEBUG
+app.UseCors("debug-cors");
+#endif
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Add OpenAPI 3.0 document serving middleware
+    // Available at: http://localhost:<port>/swagger/v1/swagger.json
+    app.UseOpenApi();
+
+    // Add web UIs to interact with the document
+    // Available at: http://localhost:<port>/swagger
+    app.UseSwaggerUi3();
 }
 
 app.UseHttpsRedirection();
